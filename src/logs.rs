2494dfbdda76216184b95e2e297b3ce0b0f87aad
saltnpepper97 <114543040+saltnpepper97@users.noreply.github.com>
2025-05-07 01:28:23 -0400

    Update logs.rs

--- src/logs.rs
+++ src/logs.rs
@@ -1,31 +1,37 @@
-use syslog::{Facility, Formatter3164, Severity};
-use std::sync::Once;
+use syslog::{BasicLogger, Facility, Formatter3164};
+use std::sync::{OnceLock, Mutex};
+use log::{info, warn, error};
 
-static INIT: Once = Once::new();
+static LOGGER: OnceLock<Mutex<BasicLogger>> = OnceLock::new();
 
 pub fn init_logger() {
-    INIT.call_once(|| {
-        let formatter = Formatter3164 {
-            facility: Facility::LOG_AUTH,
-            hostname: None,
-            process: "nexus".into(),
-            pid: std::process::id(),
-        };
+    let formatter = Formatter3164 {
+        facility: Facility::LOG_AUTH,
+        hostname: None,
+        process: "nexus".into(),
+        pid: std::process::id(),
+    };
 
-        if let Err(e) = syslog::unix(formatter) {
+    match syslog::unix(formatter) {
+        Ok(writer) => {
+            let logger = BasicLogger::new(writer);
+            let _ = log::set_boxed_logger(Box::new(logger)).map(|()| log::set_max_level(log::LevelFilter::Info));
+        }
+        Err(e) => {
             eprintln!("Failed to connect to syslog: {}", e);
         }
-    });
+    }
 }
 
 pub fn log_info(message: &str) {
-    let _ = syslog::log(Severity::Info, message);
+    info!("{}", message);
 }
 
 pub fn log_warn(message: &str) {
-    let _ = syslog::log(Severity::Warning, message);
+    warn!("{}", message);
 }
 
 pub fn log_error(message: &str) {
-    let _ = syslog::log(Severity::Err, message);
+    error!("{}", message);
 }
+
