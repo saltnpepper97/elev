7e3feb41467626c898a77cb25635691ee0b5d518
saltnpepper97 <114543040+saltnpepper97@users.noreply.github.com>
2025-05-08 10:10:40 -0400

    Update util.rs

--- src/util.rs
+++ src/util.rs
@@ -4,6 +4,7 @@ use std::time::{Duration, Instant, SystemTime, UNIX_EPOCH};
 use nix::unistd::User;
 use std::process::{Command, ExitStatus};
 use crate::logs::{log_info, log_warning, log_error};
+use crate::{Config, AuthState};
 
 /// Retrieves the groups of a user by calling the `id` command.
 pub fn get_user_groups(user: &str) -> Vec<String> {
