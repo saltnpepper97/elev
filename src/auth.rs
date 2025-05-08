3b5745e2fb1590035665d9c66fabc5df4dc464fa
saltnpepper97 <114543040+saltnpepper97@users.noreply.github.com>
2025-05-08 01:38:56 -0400

    Update auth.rs

--- src/auth.rs
+++ src/auth.rs
@@ -138,10 +138,6 @@ pub fn verify_password(user: &str, auth_state: &mut AuthState, config: &Config) 
             }
         };
 
-        if let Err(e) = ctx.set_user_prompt(Some(&format!("[ elev ] Please enter password for {}:", user))) {
-            log_error(&format!("Failed to set custom PAM prompt: {}", e));
-        }
-
         // Authenticate (prompts for password via Conversation)
         if let Err(e) = ctx.authenticate(Flag::NONE) {
             log_error(&format!("PAM authentication failed: {}", e));
