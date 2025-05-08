9f4a9bfabad19b495043e213cafca5ae7ad1419c
saltnpepper97 <114543040+saltnpepper97@users.noreply.github.com>
2025-05-08 12:28:49 -0400

    Update auth.rs

--- src/auth.rs
+++ src/auth.rs
@@ -1,11 +1,10 @@
-use rpassword::read_password;
 use pam_client2::{Context, Flag, ConversationHandler, ErrorCode};
 use std::ffi::{CStr, CString};
-use std::io::{self, Write};
+use std::io::Write;
 use std::time::{Instant, Duration};
 use crate::logs::{log_debug, log_info, log_error};
 use crate::Config;
-use crate::util::{get_user_groups, store_auth_timestamp, load_last_auth, auth_timestamp_path, get_roles_for_user};
+use crate::util::{store_auth_timestamp, load_last_auth, auth_timestamp_path, get_roles_for_user};
 
 pub struct AuthState {
     pub last_authenticated: Option<Instant>,
